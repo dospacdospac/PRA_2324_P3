bf091ddb308e81436e6d873ea7d50812f80ac39c
TFAUSEM <tfausem@alumno.upv.es@ldsic-vdi01.upvnet.upv.es>
2023-12-13 17:56:19 +0100

    Añadido Makefile y código de test de la clase TableEntry

--- TableEntry.h
+++ TableEntry.h
@@ -24,15 +24,16 @@ class TableEntry {
 	    }
 
 	    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
-		
+		return (te1.key == te2.key);	
 	    }
 
 	    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
-
+		return !(te1.key == te2.key);
 	    }
 
 	    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
-
+		out << "Key: " << te.key << ", Value: " << te.value;
+		return out;
 	    }
 	    // miembros públicos
     
